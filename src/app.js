63a569b5a9e855a76b06a4ec91cf16dba6943f8f
Thomas Davis <ThomasDavisA@gmail.com>
2020-07-08 12:47:47 -0700

    slight cors update

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const authRouter = require('./auth/auth-router')
 const koboldsRouter = require('./kobold/kobolds-router')
 const locationsRouter = require('./locations/locations-router')
 const adventuresRouter = require('./adventures/adventures-router')
-const usersRouter = require ('./users/users-router')
+const usersRouter = require('./users/users-router')
 
 const app = express()
 
@@ -17,9 +17,14 @@ const morganOption = (NODE_ENV === 'production')
     ? 'tiny'
     : 'common';
 
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200,
+}
+
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+app.use(cors(corsOptions))
 
 app.use('/api/auth', authRouter)
 app.use('/api/users', usersRouter)
